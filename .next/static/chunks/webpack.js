e9b964da699036200dd57d681b0cb0f839846ee8
Aneta <anc@praqma.net>
2023-03-24 16:11:01 +0100

    changed home.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1eea464aa37f8090"; }
+/******/ 		__webpack_require__.h = function() { return "5e5878d29d03020f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
