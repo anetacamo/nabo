3775b67a5cf3bf4ec419385df06cf844efe649b5
Aneta <anc@praqma.net>
2023-08-23 16:31:32 +0200

    added parser

--- components/Section/Section.tsx
+++ components/Section/Section.tsx
@@ -7,7 +7,15 @@ export default function section(section: Section) {
   return (
     <section className={`bg-${background} ${styles[orientation || ""]}`}>
       <h2 className={styles.title}>{title}</h2>
-      <p className={styles.text}>{text}</p>
+      {text && (
+        <p
+          className={styles.text}
+          dangerouslySetInnerHTML={{
+            __html: text,
+          }}
+        ></p>
+      )}
+
       {buttons?.map((b: Button, i: number) => (
         <button key={i} className={styles.button}>
           <a href={b?.link}>{b.name}</a>
