bb63283a33d63dfbaabf2c417e7856bb87eb3fe5
Aneta <anc@praqma.net>
2023-09-29 12:57:05 +0200

    added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
 import { AppProps } from "next/app";
 import "../styles/styles.scss";
+import Script from "next/script";
+
+const measurementId = "G-4HV5H0B5SG";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-4HV5H0B5SG"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`  
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-4HV5H0B5SG');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
