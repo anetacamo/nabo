04e49765f6bdf2a3ac136c92ce338fe56848fef2
Liam N <122264359+website-user@users.noreply.github.com>
2023-10-10 14:13:18 +0200

    Removing google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,18 +5,7 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-4HV5H0B5SG"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`  
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-4HV5H0B5SG');
-        `}
-      </Script>
+
       <Component {...pageProps} />
     </>
   );
